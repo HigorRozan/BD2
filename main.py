4e0c98ffddea3dddf8841a219a629507dbf761bc
Higor Rozan <higorb.rozan@hotmail.com>
2016-06-19 20:01:26 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,79 @@
+# encoding=UTF-8
+"""
+	Autores:Higor Rozan
+			João Bertoncini
+			Luan Andryl Silve
+
+	E-mail: higorb.rozan@gmail.com
+			jvbertoncini@gmail.com
+			luan.andryl@gmail.com
+
+	Este código recebe duas tabela R e S
+	e faz a junção deles usando o algoritmo
+	Nested Loop Blocado Bufferizado
+"""
+
+
+def convert_buffer_to_mat(buff):
+    buff = buff.split('\n')
+    buff_cache = []
+    for i in buff:
+        buff_cache.append(i.split(','))
+
+    return buff_cache
+
+
+def main():
+
+    # Atribuindo valores aos Parâmetros
+    # tabR_name = input("Nome do arquivo da tabela R externa: ")
+    # tabS_name = input("Nome do arquivo da tabela S interna: ")
+    # buff_size = int(input("Tamanho do Buffer em Bytes: "))
+    # page_size = int(input("Tamanho da Página em Bytes: "))
+
+    tabR_name = 'user'
+    tabS_name = 'dep'
+    buff_size = 500
+    page_size = 10
+
+    # Abre os arquivos das Tabelas e verifica se foram encontrados
+    # A função open recebe como parâmetro o tamanho em Bytes
+    # de blocos que devem ser lido
+    try:
+        tabR = open(tabR_name, "r", page_size)
+        tabS = open(tabS_name, "r", page_size)
+    except IOError:
+        print ("Erro - Arquivo não encontrado")
+        exit()
+
+    # Lê nos arquivo as informação da tabela
+    tabR_info = tabR.readline().split(',')
+    tabS_info = tabS.readline().split(',')
+
+    done = True
+
+    # Executa um laço até o fim do algoritmo
+    while(done):
+
+        # Lê o buffer externo
+        buffer_ext = tabR.read(buff_size)
+
+        print (buffer_ext)
+
+        cache = []
+        i = -1
+        while(buffer_ext[i] != '\n'):
+        	cache.append(buffer_ext[i])
+        	i -=1;
+        i = len(cache)-1
+        while i != -1:
+        	print (cache[i], end="")
+        	i-=1
+
+
+
+        done = False
+
+
+if __name__ == '__main__':
+    main()
