2d6686631c7af9cf3300c596ccd3d9b9545043ad
Higor Rozan <higorb.rozan@hotmail.com>
2016-06-20 15:35:10 -0300

    Multiplicação das tabela funcionando

--- main.py
+++ main.py
@@ -24,9 +24,12 @@ def arq_size(filename):
 
 
 def convert_buffer_to_mat(buff):
-	buff = buff.split('\n')
+	buff = buff.splitlines()
+	cache = []
+	for x in buff:
+		cache.append(x.split(","))
 
-	return buff
+	return cache
 
 
 def register_size(tabName):
@@ -38,18 +41,24 @@ def register_size(tabName):
 	return size
 
 
+def join_reg(regx, regy):
+	reg = []
+
+	reg.append(regx[0])
+	reg.append(regx[1])
+	reg.append(regx[2])
+	reg.append(regy[1])
+
+	return reg
+
+
 def main():
 
 	# Atribuindo valores aos Parâmetros
-	# tabR_name = input("Nome do arquivo da tabela R externa: ")
-	# tabS_name = input("Nome do arquivo da tabela S interna: ")
-	# buff_size = int(input("Tamanho do Buffer em Bytes: "))
-	# page_size = int(input("Tamanho da Página em Bytes: "))
-
-	tabR_name = 'usernew'
-	tabS_name = 'dep'
-	buff_size = 45
-	page_size = 10
+	tabR_name = input("Nome do arquivo da tabela R externa: ")
+	tabS_name = input("Nome do arquivo da tabela S interna: ")
+	buff_size = int(input("Tamanho do Buffer em Bytes: "))
+	page_size = int(input("Tamanho da Página em Bytes: "))
 
 	# Abre os arquivos das Tabelas e verifica se foram encontrados
 	# A função open recebe como parâmetro o tamanho em Bytes
@@ -66,23 +75,47 @@ def main():
 	regR_size = register_size(tabR_name)
 	regS_size = register_size(tabS_name)
 
-	buff_ext_size = round(buff_size/regR_size) * regR_size
-	buff_int_size = round(buff_size/regS_size) * regS_size 
-	
+	buff_ext_size = round(buff_size / regR_size) * regR_size
+	buff_int_size = round(buff_size / regS_size) * regS_size
+
 	# Calcula o número de leitura necessária
-	num_read_ext = m.floor(arq_size(tabR_name)/buff_ext_size)
-	num_read_int = m.floor(arq_size(tabS_name)/buff_int_size) 
+	num_read_ext = 1 + m.floor(arq_size(tabR_name) / buff_ext_size)
+	num_read_int = 1 + m.floor(arq_size(tabS_name) / buff_int_size)
 
+	# Tabela de Saida
+	out_tab = []
 
-	# Lê o buffer
-	print (regR_size)
-	print (buff_ext_size)
-	buff_ext = tabR.read(buff_ext_size)
+	# Laço externo
+	for i in range(num_read_ext):
 
-	# Tranforma em vetor
-	mat_ext = buff_ext.splitlines()
+		# Lê o buffer
+		buff_ext = tabR.read(buff_ext_size)
 
-	print (buff_ext)
+		# Tranforma em Matriz
+		mat_ext = convert_buffer_to_mat(buff_ext)
+
+		# Laço interno
+		for t in range(num_read_int):
+
+			# Lê o buffer
+			buff_int = tabS.read(buff_int_size)
+
+			# Tranforma em Matriz
+			mat_int = convert_buffer_to_mat(buff_int)
+
+			# Executa a comparação
+			for x in mat_ext:
+
+				for y in mat_int:
+
+					if (x[3] == y[0]):
+						out_tab.append(join_reg(x, y))
+
+		# Volta ai inicio da Tabela interna
+		tabS.seek(0, 0)
+
+	for reg in out_tab:
+		print (reg)
 
 
 if __name__ == '__main__':
