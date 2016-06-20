820dc2da1f8ff85b131c0c93aef3e89a50a9a023
Luan <luan@localhost.localdomain>
2016-06-20 13:06:50 -0300

    Oi :)

--- main.py
+++ main.py
@@ -16,61 +16,89 @@ import os
 
 
 def arq_size(filename):
-    # coleta os dados do arquivo
-    statinfo = os.stat(filename)
+	# coleta os dados do arquivo
+	statinfo = os.stat(filename)
 
-    return statinfo.st_size
+	return statinfo.st_size
 
 
 def convert_buffer_to_mat(buff):
-    buff = buff.split('\n')
-    buff_cache = []
-    for i in buff:
-        buff_cache.append(i.split(','))
+	buff = buff.split('\n')
+	buff_cache = []
+	for i in buff:
+		buff_cache.append(i.split(','))
 
-    return buff_cache
+
+	return buff_cache
+
+
+def register_size(tabName, page):
+	tabR = open(tabName, "r", page)
+	tabLine = tabR.readline()
+	print(tabLine)
 
 
 def main():
 
-    # Atribuindo valores aos Parâmetros
-    # tabR_name = input("Nome do arquivo da tabela R externa: ")
-    # tabS_name = input("Nome do arquivo da tabela S interna: ")
-    # buff_size = int(input("Tamanho do Buffer em Bytes: "))
-    # page_size = int(input("Tamanho da Página em Bytes: "))
+	# Atribuindo valores aos Parâmetros
+	# tabR_name = input("Nome do arquivo da tabela R externa: ")
+	# tabS_name = input("Nome do arquivo da tabela S interna: ")
+	# buff_size = int(input("Tamanho do Buffer em Bytes: "))
+	# page_size = int(input("Tamanho da Página em Bytes: "))
 
-    tabR_name = 'user'
-    tabS_name = 'dep'
-    buff_size = 20
-    page_size = 10
+	tabR_name = 'usernew'
+	tabS_name = 'dep'
+	buff_size = 45
+	page_size = 10
 
-    # Abre os arquivos das Tabelas e verifica se foram encontrados
-    # A função open recebe como parâmetro o tamanho em Bytes
-    # de blocos que devem ser lido
-    try:
-        tabR = open(tabR_name, "r", page_size)
-        tabS = open(tabS_name, "r", page_size)
-    except IOError:
-        print ("Erro - Arquivo não encontrado")
-        exit()
+	# Abre os arquivos das Tabelas e verifica se foram encontrados
+	# A função open recebe como parâmetro o tamanho em Bytes
+	# de blocos que devem ser lido
+	try:
+		tabR = open(tabR_name, "r", page_size)
+		tabS = open(tabS_name, "r", page_size)
+	except IOError:
+		print ("Erro - Arquivo não encontrado")
+		exit()
 
-    # Lê nos arquivo as informação da tabela
-    tabR_info = tabR.readline().split(',')
-    tabS_info = tabS.readline().split(',')
 
-    done = True
+	done = True
 
-    # Executa um laço até o fim do algoritmo
-    while(done):
+	#tabR_times =(round(arq_size(tabR_name)/buff_size));
+	#tabS_times =(round(arq_size(tabS_name)/buff_size));
 
-        # Lê o buffer externo
-        buffer_ext = tabR.read(buff_size)
+	print(register_size(tabR_name,page_size))
 
-        print (buffer_ext)
+	tabR_reg = round(buff_size/21)
 
-        
-        done = False
+	buffer_ext = tabR.read(42)
+	#print(buffer_ext)
+
+
+	#important
+
+	#for x in range(1,tabR_times):
+	#   buffer_ext = tabR.read(buff_size)
+	#   t.append(convert_buffer_to_mat(buffer_ext, 4))
+		
+	#   print(t[0])
+
+
+		
+		
+		
+
+	# Executa um laço até o fim do algoritmo
+	#while(done):
+
+		# Lê o buffer externo
+	  #  buffer_ext = tabR.read(buff_size)
+
+	  #  print (buffer_ext)
+
+		
+	  #  done = False
 
 
 if __name__ == '__main__':
-    main()
+	main()
